eb8e92fc5a67a47d8e0f45a8c23f4e6031668b21
torum <37834607+torum@users.noreply.github.com>
2022-12-15 11:00:08 +0900

    using WinUI3.

---
+++ ZumenSearch/Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace ZumenSearch.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ ZumenSearch/Contracts/ViewModels/INavigationAware.cs
@@ -0,0 +1,8 @@
+﻿namespace ZumenSearch.Contracts.ViewModels;
+
+public interface INavigationAware
+{
+    void OnNavigatedTo(object parameter);
+
+    void OnNavigatedFrom();
+}
