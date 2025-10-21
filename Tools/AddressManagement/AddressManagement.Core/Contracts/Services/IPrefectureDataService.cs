fba8cb12fab2f1a0ed5ae0959e50a81154476454
torum <37834607+torum@users.noreply.github.com>
2025-10-21 21:37:20 +0900

    Remove AddressManagement and moved to new MtTownAll repository.

--- Tools/AddressManagement/AddressManagement.Core/Contracts/Services/IPrefectureDataService.cs
+++
@@ -1,9 +0,0 @@
-﻿using AddressManagement.Core.Models;
-
-namespace AddressManagement.Core.Contracts.Services;
-
-public interface IPrefectureDataService
-{
-    Task<IEnumerable<Prefecture>> GetPrefectureDataAsync();
-
-}
--- Tools/AddressManagement/AddressManagement/Contracts/Services/IActivationService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AddressManagement.Contracts.Services;
-
-public interface IActivationService
-{
-    Task ActivateAsync(object activationArgs);
-}
